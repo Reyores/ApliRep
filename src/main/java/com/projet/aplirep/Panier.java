95fcf5384dc60e56d24810ee8120313232d53a7e
simon448u <thomas.simon4@etu.univ-lorraine.fr>
2022-12-08 16:19:57 +0100

    Panier

--- src/main/java/com/projet/aplirep/Panier.java
+++ src/main/java/com/projet/aplirep/Panier.java
@@ -1,4 +1,17 @@
 package com.projet.aplirep;
 
+import java.util.List;
+
 public class Panier {
+
+    List<Article> lArticles;
+
+    public void ajouterArticle(Article a){
+        lArticles.add(a);
+    }
+
+    public void retirerArticle(Article a){
+        lArticles.remove(a);
+    }
+
 }
