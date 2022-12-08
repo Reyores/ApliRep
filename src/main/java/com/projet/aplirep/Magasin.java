68eb690341b70ed7aa7a9383b86ff1a0fa1683d3
simon448u <thomas.simon4@etu.univ-lorraine.fr>
2022-12-08 16:00:26 +0100

    Magasin

---
+++ src/main/java/com/projet/aplirep/Magasin.java
@@ -0,0 +1,13 @@
+package com.projet.aplirep;
+
+import java.util.List;
+
+public class Magasin {
+
+    List<Article> lArticle;
+
+    public Magasin(List<Article> lA){
+        lArticle=lA;
+    }
+
+}
