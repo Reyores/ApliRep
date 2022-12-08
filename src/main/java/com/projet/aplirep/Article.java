653e7aabb06194757c0aad08bccce45a21754bda
simon448u <thomas.simon4@etu.univ-lorraine.fr>
2022-12-08 15:57:34 +0100

    Article

---
+++ src/main/java/com/projet/aplirep/Article.java
@@ -0,0 +1,15 @@
+package com.projet.aplirep;
+
+public class Article {
+
+    String nom;
+    double prix;
+    String image;
+
+    public Article(String n,double p,String i){
+        nom=n;
+        prix=p;
+        image=i;
+    }
+
+}
