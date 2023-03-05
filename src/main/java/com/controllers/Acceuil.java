67ef5537246cef44962bb87f8c31e258439cf586
Reyores <royeralexandre@outlook.fr>
2023-03-05 15:58:45 +0100

    IG

--- src/main/java/com/controllers/Acceuil.java
+++ src/main/java/com/controllers/Acceuil.java
@@ -1,4 +1,24 @@
 package com.controllers;
 
-public class Acceuil {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Acceuil extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
